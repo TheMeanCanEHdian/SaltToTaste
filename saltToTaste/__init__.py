a7ee63ad60033833d0cf3aa078c715cd6fa50980
TheMeanCanEHdian <drivard100@gmail.com>
2020-01-28 09:24:50 -0600

    Gracefully exit if flask secret_key is missing from config.ini

--- saltToTaste/__init__.py
+++ saltToTaste/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from flask import Flask, Blueprint
 import flask_whooshalchemy as wa
 from saltToTaste.extensions import db, login_manager
@@ -6,7 +7,7 @@ from saltToTaste.views.main import main
 from saltToTaste.views.api import api
 from saltToTaste.models import Recipe, Ingredient, Note, User, Tag, Direction
 from saltToTaste.database_handler import add_all_recipes, update_recipes, add_new_recipes, remove_missing_recipes, db_cleanup
-from saltToTaste.file_handler import recipe_importer, create_default_config
+from saltToTaste.file_handler import recipe_importer, create_default_configfile
 from saltToTaste.parser_handler import argparser_results, configparser_results
 
 def create_app(config_file='settings.py'):
@@ -25,11 +26,15 @@ def create_app(config_file='settings.py'):
     app.config['WHOOSH_ANALYZER'] = 'StemmingAnalyzer'
 
     if not os.path.isfile(app.config['CONFIG_INI']):
-        create_default_config(app.config['CONFIG_INI'])
+        create_default_configfile(app.config['CONFIG_INI'])
 
     config = configparser_results(app.config['CONFIG_INI'])
 
-    app.config['SECRET_KEY'] = config['flask']['secret_key']
+    try:
+        app.config['SECRET_KEY'] = config.get('flask', 'secret_key')
+    except TypeError:
+        print ('Error: Could not find Flask secret_key in config.ini.')
+        sys.exit()
 
     # Register blueprints
     app.register_blueprint(main)
