3dae3896bfdf419e07c8832fc4c0a424a2b2f549
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-03 08:02:21 -0500

    Use URL path strategy for Flutter web.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'configure_nonweb.dart' if (dart.library.html) 'configure_web.dart';
 import 'salt_to_taste.dart';
 
 void main() {
+  configureApp();
   runApp(
     SaltToTaste(),
   );
