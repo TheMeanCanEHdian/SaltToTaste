186e192715b79d3a3256d7063eb350bde505ffa1
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-03 09:31:26 -0500

    Small styling adjustments to NavBar.

--- lib/core/widgets/nav_bar.dart
+++ lib/core/widgets/nav_bar.dart
@@ -17,59 +17,81 @@ class _NavBarState extends State<NavBar> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
+        borderRadius: BorderRadius.circular(4),
         child: Container(
           color: const Color.fromRGBO(145, 0, 6, 1),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  height: 40,
-                  child: Image.asset('assets/logo/logo-banner-40.png'),
-                ),
-                Row(
-                  children: [
-                    InkWell(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const _NavBarLogo(),
+              Row(
+                children: [
+                  Material(
+                    color: Colors.transparent,
+                    child: InkWell(
                       onHover: (value) {
                         setState(() {
                           _isHovering['login'] = value;
                         });
                       },
                       onTap: () {},
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          color: _isHovering['login']!
-                              ? Colors.lightGreen
-                              : Colors.white,
+                      child: Container(
+                        height: 56,
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: const Center(
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                    PopupMenuButton(
-                      icon: const Icon(
-                        Icons.more_vert,
-                        color: Colors.white,
+                  ),
+                  SizedBox(
+                    height: 56,
+                    child: Material(
+                      color: Colors.transparent,
+                      child: PopupMenuButton(
+                        icon: const Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                        ),
+                        itemBuilder: (context) => [
+                          const PopupMenuItem(
+                            child: Text('Item 1'),
+                          ),
+                          const PopupMenuItem(
+                            child: Text('Item 2'),
+                          ),
+                          const PopupMenuItem(
+                            child: Text('Item 3'),
+                          ),
+                        ],
                       ),
-                      itemBuilder: (context) => [
-                        const PopupMenuItem(
-                          child: Text('Item 1'),
-                        ),
-                        const PopupMenuItem(
-                          child: Text('Item 2'),
-                        ),
-                        const PopupMenuItem(
-                          child: Text('Item 3'),
-                        ),
-                      ],
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _NavBarLogo extends StatelessWidget {
+  const _NavBarLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 40,
+        child: Image.asset('assets/logo/logo-banner-40.png'),
       ),
     );
   }
