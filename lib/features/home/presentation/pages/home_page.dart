1ebf57a1e4a7555d4ea090d322bc5da0ef8f2d5a
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-03 11:45:00 -0500

    Move NavBar to stack to float over content.

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -7,14 +7,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size(screenSize.width, 1000),
-        child: NavBar(),
+      body: Stack(
+        children: [
+          GridView.count(
+            padding: const EdgeInsets.only(
+              top: 64,
+              left: 4,
+              right: 4,
+            ),
+            crossAxisCount: 4,
+            childAspectRatio: 3 / 2,
+            children: const [],
+          ),
+          const NavBar(),
+        ],
       ),
-      body: Container(),
     );
   }
 }
