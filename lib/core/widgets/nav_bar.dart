88b3a656d35da5faaab5b87d8c0431dbbbac81cd
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-03 10:45:47 -0500

    Use Card for NavBar.

--- lib/core/widgets/nav_bar.dart
+++ lib/core/widgets/nav_bar.dart
@@ -14,68 +14,71 @@ class _NavBarState extends State<NavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(4),
-        child: Container(
-          color: const Color.fromRGBO(145, 0, 6, 1),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const _NavBarLogo(),
-              Row(
-                children: [
-                  Material(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(4),
+      child: Card(
+        margin: const EdgeInsets.only(
+          top: 8,
+          right: 8,
+          left: 8,
+        ),
+        elevation: 0.5,
+        color: const Color.fromRGBO(145, 0, 6, 1),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const _NavBarLogo(),
+            Row(
+              children: [
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onHover: (value) {
+                      setState(() {
+                        _isHovering['login'] = value;
+                      });
+                    },
+                    onTap: () {},
+                    child: Container(
+                      height: 56,
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: const Center(
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 56,
+                  child: Material(
                     color: Colors.transparent,
-                    child: InkWell(
-                      onHover: (value) {
-                        setState(() {
-                          _isHovering['login'] = value;
-                        });
-                      },
-                      onTap: () {},
-                      child: Container(
-                        height: 56,
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: const Center(
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
+                    child: PopupMenuButton(
+                      icon: const Icon(
+                        Icons.more_vert,
+                        color: Colors.white,
                       ),
+                      itemBuilder: (context) => [
+                        const PopupMenuItem(
+                          child: Text('Item 1'),
+                        ),
+                        const PopupMenuItem(
+                          child: Text('Item 2'),
+                        ),
+                        const PopupMenuItem(
+                          child: Text('Item 3'),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    height: 56,
-                    child: Material(
-                      color: Colors.transparent,
-                      child: PopupMenuButton(
-                        icon: const Icon(
-                          Icons.more_vert,
-                          color: Colors.white,
-                        ),
-                        itemBuilder: (context) => [
-                          const PopupMenuItem(
-                            child: Text('Item 1'),
-                          ),
-                          const PopupMenuItem(
-                            child: Text('Item 2'),
-                          ),
-                          const PopupMenuItem(
-                            child: Text('Item 3'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
