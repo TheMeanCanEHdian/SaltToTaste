93a8baeb01ce8dc0981503048c2cdc09b666d1a6
Derek Rivard <drivard100@gmail.com>
2021-07-06 17:36:23 -0500

    Add bloc for Home.

--- lib/features/home/presentation/bloc/home_state.dart
+++ lib/features/home/presentation/bloc/home_state.dart
@@ -1,8 +1,29 @@
 part of 'home_bloc.dart';
+
 abstract class HomeState extends Equatable {
   const HomeState();
 }
+
 class HomeInitial extends HomeState {
   @override
   List<Object> get props => [];
+}
+
+class HomeInProgress extends HomeState {
+  @override
+  List<Object?> get props => [];
+}
+
+class HomeSuccess extends HomeState {
+  final List<Recipe> recipes;
+
+  HomeSuccess({required this.recipes});
+
+  @override
+  List<Object?> get props => [recipes];
+}
+
+class HomeFailure extends HomeState {
+  @override
+  List<Object?> get props => [];
 }
