22dde6e17d70f29f3ec42b8ae0649f86ef569fca
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-12 16:05:33 -0500

    Update RecipeCard styling and navigation.

--- frontend/lib/features/home/presentation/widgets/recipe_card.dart
+++ frontend/lib/features/home/presentation/widgets/recipe_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../domain/entities/recipe.dart';
 
-class RecipeCard extends StatelessWidget {
+class RecipeCard extends StatefulWidget {
   final Recipe recipe;
 
   const RecipeCard({
@@ -11,80 +11,102 @@ class RecipeCard extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _RecipeCardState createState() => _RecipeCardState();
+}
+
+class _RecipeCardState extends State<RecipeCard> {
+  bool isHover = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 0.5,
       color: Colors.black12,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(4),
-        // Material is needed inside ClipRRect for InkWell to respect borderRadius
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: () {},
-            child: Stack(
-              children: [
-                Positioned.fill(
-                  child: Image.network(
-                    'http://127.0.0.1:5000/api/image/${recipe.titleSanitized}',
-                    fit: BoxFit.cover,
-                  ),
+        // Using InkWell for built in onHover
+        child: InkWell(
+          onHover: (value) {
+            setState(() {
+              isHover = value;
+            });
+          },
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              '/recipe/${widget.recipe.titleSanitized}',
+            );
+          },
+          child: Stack(
+            children: [
+              // Ink.image allows for the ink effect on an image rather than fron InkWell
+              Ink.image(
+                image: NetworkImage(
+                  'http://127.0.0.1:5000/api/image/${widget.recipe.titleSanitized}',
                 ),
-                Positioned.fill(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        stops: [0, 0.6],
-                        colors: [Colors.black, Colors.transparent],
-                      ),
+                fit: BoxFit.cover,
+              ),
+              const Positioned.fill(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      stops: [0, 0.6],
+                      colors: [Colors.black, Colors.transparent],
                     ),
                   ),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 8,
-                        right: 8,
-                      ),
-                      child: Text(
-                        recipe.title,
-                        style: const TextStyle(
-                          fontSize: 22,
-                          color: Colors.white,
-                        ),
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                      ),
+              ),
+              Positioned.fill(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: isHover ? Colors.black26 : Colors.transparent,
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 8,
+                      right: 8,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 4,
-                        left: 8,
-                        bottom: 8,
+                    child: Text(
+                      widget.recipe.title,
+                      style: const TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
                       ),
-                      child: Row(
-                        children: const [
-                          Padding(
-                            padding: EdgeInsets.only(right: 4),
-                            child: Chip(
-                              label: Text('chip'),
-                            ),
-                          ),
-                          Chip(
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 4,
+                      left: 8,
+                      bottom: 8,
+                    ),
+                    child: Row(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(right: 4),
+                          child: Chip(
                             label: Text('chip'),
                           ),
-                        ],
-                      ),
+                        ),
+                        Chip(
+                          label: Text('chip'),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
