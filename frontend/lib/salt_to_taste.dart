9d2ae1e27dd923266f4a3bc25745d5ce0e7fc051
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-12 16:04:33 -0500

    Add Fluro for route navigation.

--- frontend/lib/salt_to_taste.dart
+++ frontend/lib/salt_to_taste.dart
@@ -1,6 +1,8 @@
+import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 
 import 'features/home/presentation/pages/home_page.dart';
+import 'features/recipe/presentation/pages/recipe_page.dart';
 
 class SaltToTaste extends StatefulWidget {
   SaltToTaste({Key? key}) : super(key: key);
@@ -11,13 +13,49 @@ class SaltToTaste extends StatefulWidget {
 
 class _SaltToTasteState extends State<SaltToTaste> {
   @override
+  void initState() {
+    super.initState();
+    Flurorouter.setupRouter();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Salt To Taste',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const HomePage(),
+      initialRoute: '/',
+      onGenerateRoute: Flurorouter.router.generator,
+    );
+  }
+}
+
+class Flurorouter {
+  static final FluroRouter router = FluroRouter();
+
+  static final Handler _homePageHandler = Handler(
+    handlerFunc: (context, parameters) => const HomePage(),
+  );
+
+  static final Handler _recipePageHandler = Handler(
+    handlerFunc: (context, params) => RecipePage(
+      titleSanitized: params['titleSanitized']![0],
+    ),
+  );
+
+  static void setupRouter() {
+    const transitionType = TransitionType.none;
+
+    router.define(
+      '/',
+      handler: _homePageHandler,
+      transitionType: transitionType,
+    );
+    router.define(
+      '/recipe/:titleSanitized',
+      handler: _recipePageHandler,
+      transitionType: transitionType,
     );
   }
 }
