231a0e56f8d38e2815324ec192657fc4e3ed80aa
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-13 13:37:16 -0500

    Allow images to be resized.

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from flask import Flask, send_file, send_from_directory
 from flask_cors import CORS
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with, reqparse
 
 from core.database.datasources.database import Database
 from core.database.models.recipe_model import Recipe
+from features.image.image_data_source import ImageDataSource
 from features.recipes.recipe_data_source import RecipeFiles, RecipeDB
 from extensions import db
 
@@ -24,8 +24,19 @@ db.create_all()
 database_handler = Database()
 recipe_files_handler = RecipeFiles()
 recipe_database_handler = RecipeDB()
+image_data_source = ImageDataSource()
 
 recipe_database_handler.add_recipe_files_to_db(recipe_files_handler.load())
+
+image_req_args = reqparse.RequestParser()
+image_req_args.add_argument(
+    'width',
+    type=int,
+)
+image_req_args.add_argument(
+    'height',
+    type=int,
+)
 
 
 class Recipe(Resource):
@@ -61,14 +72,34 @@ class Recipes(Resource):
 
 
 class Image(Resource):
-    #TODO: Need to allow images to be resized
     def get(self, title_sanitized):
-        image_name = database_handler.get_recipe_image(title_sanitized)
+        args = image_req_args.parse_args()
+        width = args['width']
+        height = args['height']
 
-        return send_from_directory(
-            app.config['RECIPE_IMAGES'],
-            filename=image_name,
-        )
+        # Return error on bad arguments
+        if ((width and not height) or (height and not width)):
+            return {
+                'result': 'error',
+                'message':
+                'When setting width and height both must be specified.',
+            }, 400
+
+        # Get Pillow image
+        image = image_data_source.get_image(title_sanitized)
+        
+        # If width and height are set resize image
+        if (width and height):
+            image_data_source.resize_image(
+                image,
+                width,
+                height,
+            )
+
+        # Save image as BytesIO
+        img_io = image_data_source.save_image_as_bytes(image)
+
+        return send_file(img_io, mimetype='image/jpeg')
 
 
 api.add_resource(RecipeList, '/api/recipe_list')
