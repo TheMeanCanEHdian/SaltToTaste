8517b0293b92fa48597d2442f6f93bcd1bd7852c
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-12 19:12:04 -0500

    Use Tag in Recipe entity and model.

--- frontend/lib/features/home/data/models/recipe_model.dart
+++ frontend/lib/features/home/data/models/recipe_model.dart
@@ -1,21 +1,31 @@
 import '../../domain/entities/recipe.dart';
+import '../../domain/entities/tag.dart';
+import 'tag_model.dart';
 
 class RecipeModel extends Recipe {
   RecipeModel({
     required int id,
     required String title,
     String titleSanitized = '',
+    List<Tag> tags = const [],
   }) : super(
           id: id,
           title: title,
           titleSanitized: titleSanitized,
+          tags: tags,
         );
 
   factory RecipeModel.fromJson(Map<String, dynamic> json) {
+    List<Tag> tags = [];
+    for (var tag in json['tags']) {
+      tags.add(TagModel.fromJson(tag));
+    }
+
     return RecipeModel(
       id: json['id'],
       title: json['title'],
       titleSanitized: json['title_sanitized'],
+      tags: tags,
     );
   }
 }
--- frontend/lib/features/home/domain/entities/recipe.dart
+++ frontend/lib/features/home/domain/entities/recipe.dart
@@ -1,14 +1,18 @@
 import 'package:equatable/equatable.dart';
 
+import 'tag.dart';
+
 class Recipe extends Equatable {
   final int id;
   final String title;
   final String titleSanitized;
+  final List<Tag> tags;
 
   Recipe({
     required this.id,
     required this.title,
     this.titleSanitized = '',
+    this.tags = const [],
   });
 
   @override
@@ -16,5 +20,6 @@ class Recipe extends Equatable {
         id,
         title,
         titleSanitized,
+        tags,
       ];
 }
