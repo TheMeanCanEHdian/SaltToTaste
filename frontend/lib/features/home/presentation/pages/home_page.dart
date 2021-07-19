28b5885e81be7a2a4dee846c5cff405bf3d15aac
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-19 14:25:22 -0500

    Refactor HomePage to display loading indicator.
    Also display message when recipes is empty.

--- frontend/lib/features/home/presentation/pages/home_page.dart
+++ frontend/lib/features/home/presentation/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/widgets/nav_bar.dart';
 import '../../../../injection_container.dart' as di;
+import '../../../recipe/domain/entities/recipe.dart';
 import '../bloc/home_bloc.dart';
 import '../widgets/recipe_card.dart';
 
@@ -21,52 +22,94 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class HomePageContent extends StatefulWidget {
+class HomePageContent extends StatelessWidget {
   const HomePageContent({Key? key}) : super(key: key);
 
   @override
-  _HomePageContentState createState() => _HomePageContentState();
-}
-
-class _HomePageContentState extends State<HomePageContent> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<HomeBloc, HomeState>(
-        builder: (context, state) {
-          if (state is HomeInProgress) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (state is HomeFailure) {
-            return const Text('Failure');
-          }
-          if (state is HomeSuccess) {
-            return Stack(
-              children: [
-                GridView.count(
-                  cacheExtent: 7000,
-                  padding: const EdgeInsets.only(
-                    top: 64,
-                    left: 4,
-                    right: 4,
-                  ),
-                  crossAxisCount: 4,
-                  childAspectRatio: 3 / 2,
-                  children: state.recipes
-                      .map(
-                        (recipe) => RecipeCard(recipe: recipe),
-                      )
-                      .toList(),
-                ),
-                const NavBar(),
-              ],
-            );
-          }
-          return const Text('Unknown bloc state');
-        },
+      body: Stack(
+        children: [
+          BlocBuilder<HomeBloc, HomeState>(
+            builder: (context, state) {
+              if (state is HomeInProgress) {
+                if (state.recipes.isEmpty) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return _HomeGrid(recipes: state.recipes);
+              }
+              if (state is HomeSuccess) {
+                if (state.recipes.isEmpty) {
+                  return const Center(
+                    child: Text('No recipes found'),
+                  );
+                }
+                return _HomeGrid(recipes: state.recipes);
+              }
+              if (state is HomeFailure) {
+                return const Center(
+                  child: Text('Failed to load recipes'),
+                );
+              }
+              return const Center(
+                child: Text('ERROR: Unknown bloc state'),
+              );
+            },
+          ),
+          BlocBuilder<HomeBloc, HomeState>(
+            builder: (context, state) {
+              return NavBar(
+                loading: state is HomeInProgress && state.recipes.isNotEmpty,
+              );
+            },
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class _HomeGrid extends StatefulWidget {
+  final List<Recipe> recipes;
+
+  const _HomeGrid({
+    Key? key,
+    required this.recipes,
+  }) : super(key: key);
+
+  @override
+  __HomeGridState createState() => __HomeGridState();
+}
+
+class __HomeGridState extends State<_HomeGrid> {
+  static bool _isSmallScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width < 800;
+  static bool _isMediumScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 800 &&
+      MediaQuery.of(context).size.width <= 1200;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      cacheExtent: 7000,
+      padding: const EdgeInsets.only(
+        top: 64,
+        left: 4,
+        right: 4,
+      ),
+      crossAxisCount: _isSmallScreen(context)
+          ? 1
+          : _isMediumScreen(context)
+              ? 3
+              : 4,
+      childAspectRatio: 3 / 2,
+      children: widget.recipes
+          .map(
+            (recipe) => RecipeCard(recipe: recipe),
+          )
+          .toList(),
     );
   }
 }
