3ff76b5d8f097c52af42fe48c144c22ef756d9ee
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-19 14:13:01 -0500

    Remove rounded corners and margin on NavBar.

--- frontend/lib/core/widgets/nav_bar.dart
+++ frontend/lib/core/widgets/nav_bar.dart
@@ -5,67 +5,58 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(4),
-      child: Card(
-        margin: const EdgeInsets.only(
-          top: 8,
-          right: 8,
-          left: 8,
-        ),
-        elevation: 0.5,
-        color: const Color.fromRGBO(145, 0, 6, 1),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const _NavBarLogo(),
-            Row(
-              children: [
-                Material(
+    return Container(
+      color: const Color.fromRGBO(145, 0, 6, 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const _NavBarLogo(),
+          Row(
+            children: [
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: 56,
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: const Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 56,
+                child: Material(
                   color: Colors.transparent,
-                  child: InkWell(
-                    onTap: () {},
-                    child: Container(
-                      height: 56,
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: const Center(
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                  child: PopupMenuButton(
+                    icon: const Icon(
+                      Icons.more_vert,
+                      color: Colors.white,
                     ),
+                    itemBuilder: (context) => [
+                      const PopupMenuItem(
+                        child: Text('Item 1'),
+                      ),
+                      const PopupMenuItem(
+                        child: Text('Item 2'),
+                      ),
+                      const PopupMenuItem(
+                        child: Text('Item 3'),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(
-                  height: 56,
-                  child: Material(
-                    color: Colors.transparent,
-                    child: PopupMenuButton(
-                      icon: const Icon(
-                        Icons.more_vert,
-                        color: Colors.white,
-                      ),
-                      itemBuilder: (context) => [
-                        const PopupMenuItem(
-                          child: Text('Item 1'),
-                        ),
-                        const PopupMenuItem(
-                          child: Text('Item 2'),
-                        ),
-                        const PopupMenuItem(
-                          child: Text('Item 3'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -76,24 +67,18 @@ class _NavBarLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(4),
-        bottomLeft: Radius.circular(4),
-      ),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: () {
-            Navigator.pushNamed(context, '/');
-          },
-          hoverColor: Colors.transparent,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: SizedBox(
-              height: 40,
-              child: Image.asset('assets/logo/logo-banner-40.png'),
-            ),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, '/');
+        },
+        hoverColor: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: SizedBox(
+            height: 40,
+            child: Image.asset('assets/logo/logo-banner-40.png'),
           ),
         ),
       ),
