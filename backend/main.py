665017ff7be32e5091700ca424da334f58b1e318
TheMeanCanEHdian <drivard100@gmail.com>
2021-07-19 21:30:44 -0500

    Create settings endpoints for get and put.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, send_file, send_from_directory
 from flask_cors import CORS
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with, reqparse
+import sys
 
 from core.database.datasources.database import Database
 from core.database.models.recipe_model import Recipe
 from features.image.image_data_source import ImageDataSource
 from features.recipes.recipe_data_source import RecipeFiles, RecipeDB
+from features.settings.settings_data_source import SettingsDataSource
 from extensions import db
 
 app = Flask(__name__)
@@ -25,8 +27,14 @@ database_handler = Database()
 recipe_files_handler = RecipeFiles()
 recipe_database_handler = RecipeDB()
 image_data_source = ImageDataSource()
+settings_data_source = SettingsDataSource()
 
-recipe_database_handler.add_recipe_files_to_db(recipe_files_handler.load())
+if not settings_data_source.verify_config_file():
+    print('Failed to load config file')
+    sys.exit()
+
+recipe_dict_list = recipe_files_handler.load()
+recipe_database_handler.add_recipe_files_to_db(recipe_dict_list)
 
 image_req_args = reqparse.RequestParser()
 image_req_args.add_argument(
@@ -37,6 +45,54 @@ image_req_args.add_argument(
     'height',
     type=int,
 )
+
+settings_put_args = reqparse.RequestParser()
+settings_put_args.add_argument(
+    'section',
+    help='Section argument is required.',
+    required=True,
+)
+settings_put_args.add_argument(
+    'setting',
+    help='Setting argument is required.',
+    required=True,
+)
+settings_put_args.add_argument(
+    'value',
+    help='Value argument is required.',
+    required=True,
+)
+
+
+class Image(Resource):
+    def get(self, title_sanitized):
+        args = image_req_args.parse_args()
+        width = args['width']
+        height = args['height']
+
+        # Return error on bad arguments
+        if ((width and not height) or (height and not width)):
+            return {
+                'result': 'error',
+                'message':
+                'When setting width and height both must be specified.',
+            }, 400
+
+        # Get Pillow image
+        image = image_data_source.get_image(title_sanitized)
+
+        # If width and height are set resize image
+        if (width and height):
+            image_data_source.resize_image(
+                image,
+                width,
+                height,
+            )
+
+        # Save image as BytesIO
+        img_io = image_data_source.save_image_as_bytes(image)
+
+        return send_file(img_io, mimetype='image/jpeg')
 
 
 class Recipe(Resource):
@@ -71,41 +127,32 @@ class Recipes(Resource):
         }, 200
 
 
-class Image(Resource):
-    def get(self, title_sanitized):
-        args = image_req_args.parse_args()
-        width = args['width']
-        height = args['height']
+class Settings(Resource):
+    def get(self):
+        return settings_data_source.get_config_dict()
 
-        # Return error on bad arguments
-        if ((width and not height) or (height and not width)):
+    def put(self):
+        args = settings_put_args.parse_args()
+        section = args['section']
+        setting = args['setting']
+        value = args['value']
+
+        result = settings_data_source.update_setting(section, setting, value)
+
+        if result:
+            return {'result': 'success'}
+        else:
             return {
                 'result': 'error',
-                'message':
-                'When setting width and height both must be specified.',
-            }, 400
-
-        # Get Pillow image
-        image = image_data_source.get_image(title_sanitized)
-        
-        # If width and height are set resize image
-        if (width and height):
-            image_data_source.resize_image(
-                image,
-                width,
-                height,
-            )
-
-        # Save image as BytesIO
-        img_io = image_data_source.save_image_as_bytes(image)
-
-        return send_file(img_io, mimetype='image/jpeg')
+                'message': 'Failed to update setting',
+            }
 
 
+api.add_resource(Image, '/api/image/<string:title_sanitized>')
 api.add_resource(RecipeList, '/api/recipe_list')
 api.add_resource(Recipes, '/api/recipes')
 api.add_resource(Recipe, '/api/recipe/<string:title_sanitized>')
-api.add_resource(Image, '/api/image/<string:title_sanitized>')
+api.add_resource(Settings, '/api/settings')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
